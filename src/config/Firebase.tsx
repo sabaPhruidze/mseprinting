66ebc55083197c043642a166e7fe285f0d999a4a
Saba Phruidze <sabaphruidze01@gmail.com>
2024-06-07 10:49:54 +0400

    and I made it . currently if someone registers all the information will be uploaded in the database. next is to make login and register switcher and in login when someone registers, must have an opportunity to login and I have to receive the other information in order to make it possible to send a qoute or a file easily

--- src/config/Firebase.tsx
+++ src/config/Firebase.tsx
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDMFV6mkUm5-8l45gnJl_Wm26ISDxHwots",
@@ -10,3 +12,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
