0e932da9bd77ed381f18f50b0b420d6a89b73812
Saba Phruidze <sabaphruidze01@gmail.com>
2024-06-16 13:03:32 +0400

    Currently I uploaded and received data about cards. what I am doing currently is to fully upload images and text on the database and after that I might change some photos, as well as add a field of products and services where all products will be written

--- src/importantparts/WWDCCard.tsx
+++ src/importantparts/WWDCCard.tsx
@@ -1,27 +1,41 @@
-import { useMemo } from "react";
+import { useMemo, useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import { CardContainer } from "../style/HomeStyles";
-import CardData from "../data/CardData";
+// import CardData from "../data/CardData";
+
+import { WWDCCardType, fetchWWDCCardData } from "../data/CardData";
 
 function WWDCCard() {
-  const DATA = useMemo(() => CardData(), []);
+  const [WWDCCardMainData, setWWDCCardMainData] = useState<WWDCCardType[]>([]);
+  // const DATA = useMemo(() => CardData(), []);
 
+  useEffect(() => {
+    const getCarouselData = async () => {
+      const data = await fetchWWDCCardData();
+
+      if (data && data.length > 0) {
+        setWWDCCardMainData(data);
+      }
+    };
+
+    getCarouselData();
+  }, []);
   return (
     <Container>
       <Row xs={1} sm={2} md={3} className="g-4">
-        {DATA.map((card, idx) => (
+        {WWDCCardMainData.map((card, idx) => (
           <Col key={idx}>
             <CardContainer className="h-100">
               <CardContainer.Img
                 variant="top"
-                src={card.imgSrc}
+                src={card.image}
                 style={{ height: 180, objectFit: "cover" }}
               />
               <CardContainer.Body>
                 <CardContainer.Title>{card.title}</CardContainer.Title>
-                <CardContainer.Text>{card.content}</CardContainer.Text>
+                <CardContainer.Text>{card.text}</CardContainer.Text>
               </CardContainer.Body>
             </CardContainer>
           </Col>
