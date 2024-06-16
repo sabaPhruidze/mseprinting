bbe9c051b27178eeadd291bfd2cd584d8cbb433f
Saba Phruidze <sabaphruidze01@gmail.com>
2024-06-16 19:14:55 +0400

    I made the products and services, visually better . also added two data for signs and printing. I will add other for other time but I like how it visually looks like .I will make sure not to lag it and correct whats needed to be corrected

--- src/style/ProductServicesContainerStyles.tsx
+++ src/style/ProductServicesContainerStyles.tsx
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 import { RowFlex, ColumnFlex } from "./GlobalStyle";
+
 export const ProductsServicesContainerStyle = styled(RowFlex)`
   width: 700px;
-  height: 500px;
+  height: 400px;
   background-color: ${(props) => props.theme.White};
   position: absolute;
   top: 140px;
@@ -12,12 +13,18 @@ export const ProductsServicesContainerStyle = styled(RowFlex)`
   padding: 10px;
   box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
 `;
-//left side
+
+// Left side
 export const LeftSideContainer = styled(ColumnFlex)`
   width: 250px;
   height: 100%;
+  border-right: 1px solid ${(props) => props.theme.MediumBlue};
+  justify-content: flex-start;
 `;
+
 export const LeftSideText = styled.button`
+  text-align: left;
+  margin-bottom: 10px;
   width: 100%;
   font-size: 16px;
   border: 0;
@@ -25,13 +32,45 @@ export const LeftSideText = styled.button`
   color: ${(props) => props.theme.MediumBlue};
   transition: 0.3s;
   white-space: nowrap;
+  cursor: pointer;
   &:hover {
-    transform: scale(1.1);
     font-weight: 500;
+    transform: scale(1.1);
+    padding-left: 10px;
   }
 `;
-//right side
-export const RightSideContainer = styled(ColumnFlex)`
-  width: 450px;
+
+// Right side
+export const RightSideContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(
+    auto-fill,
+    minmax(150px, 1fr)
+  ); // Use auto-fill for responsive grid
+  grid-auto-rows: min-content; // Adjust row height to content
+  gap: 10px; // Reduced gap for closer spacing
+  width: calc(100% - 260px); // Adjust width to leave some space
   height: 100%;
+  overflow-y: auto;
+  padding: 15px;
+  font-size: 14px; // Default font size
+  @media (max-width: 700px) {
+    font-size: 12px; // Smaller font size for smaller screens
+  }
+`;
+
+export const RightSideText = styled.div`
+  background-color: ${(props) => props.theme.LightGray};
+  border: 1px solid ${(props) => props.theme.MediumBlue};
+  border-radius: 5px;
+  padding: 5px 10px; // Padding: top/bottom 5px, left/right 10px
+  text-align: center;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  cursor: pointer;
+  &:hover {
+    background-color: ${(props) => props.theme.MediumBlue};
+    color: ${(props) => props.theme.White};
+  }
 `;
