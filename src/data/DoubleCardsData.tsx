5ba0489d23330af9159d76f26b38bb9bc7105e86
Saba Phruidze <sabaphruidze01@gmail.com>
2024-06-23 18:48:44 +0400

    I filtered the Data code , so from now on I will receive data in much easier way from the firebase

--- src/data/DoubleCardsData.tsx
+++ src/data/DoubleCardsData.tsx
@@ -1,63 +1,20 @@
-import { doc, getDoc } from "firebase/firestore";
-import { db } from "../config/Firebase";
-
-export interface DoubleCardsDarkType {
-  imageDark: string;
-  imageRQ: string | null;
-  link: string | null;
-  title: string | null;
-}
+import { DoubleCardsDarkType, DoubleCardsLightType } from "../types/DataTypes";
+import { GeneralizedFetch } from "../importantparts/GeneralizedFetch";
 
 export const fetchDoubleCardsDarkData =
   async (): Promise<DoubleCardsDarkType> => {
-    try {
-      const docRef = doc(db, "home", "doubleCardsDark");
-
-      const docSnap = await getDoc(docRef);
-
-      if (docSnap.exists()) {
-        const data = docSnap.data() as DoubleCardsDarkType;
-        return {
-          imageDark: data.imageDark,
-          imageRQ: data.imageRQ,
-          link: data.link,
-          title: data.title,
-        };
-      } else {
-        return { imageDark: "", imageRQ: null, link: null, title: null };
-      }
-    } catch (error) {
-      console.error("Error fetching header main logo: ", error);
-      return { imageDark: "", imageRQ: null, link: null, title: null };
-    }
+    const data = await GeneralizedFetch<DoubleCardsDarkType>(
+      "home",
+      "doubleCardsDark"
+    );
+    return data || { imageDark: "", imageRQ: null, link: null, title: null };
   };
-export interface DoubleCardsLightType {
-  imageLight: string;
-  imageSF: string | null;
-  link: string | null;
-  title: string | null;
-}
 
 export const fetchDoubleCardsLightData =
   async (): Promise<DoubleCardsLightType> => {
-    try {
-      const docRef = doc(db, "home", "doubleCardsLight");
-
-      const docSnap = await getDoc(docRef);
-
-      if (docSnap.exists()) {
-        const data = docSnap.data() as DoubleCardsLightType;
-        return {
-          imageLight: data.imageLight,
-          imageSF: data.imageSF,
-          link: data.link,
-          title: data.title,
-        };
-      } else {
-        return { imageLight: "", imageSF: null, link: null, title: null };
-      }
-    } catch (error) {
-      console.error("Error fetching header main logo: ", error);
-      return { imageLight: "", imageSF: null, link: null, title: null };
-    }
+    const data = await GeneralizedFetch<DoubleCardsLightType>(
+      "home",
+      "doubleCardsLight"
+    );
+    return data || { imageLight: "", imageSF: null, link: null, title: null };
   };
--- src/types/DataTypes.tsx
+++ src/types/DataTypes.tsx
@@ -1,5 +1,64 @@
+//header
+export interface HMenuType {
+  page: string;
+  path: string;
+}
+export interface LogoType {
+  logo: string | null;
+}
+//footer
+
 //categories
 
+//Home
+
+//CarouselDataType
+
+export interface CarouselType {
+  image: string;
+  alt: string;
+  title: string;
+  text: string;
+  link: string;
+}
+
+//WWDCCardType
+
+export interface WWDCCardType {
+  image: string;
+  link: string;
+  text: string;
+  title: string;
+}
+
+export interface WWDCSpecialitiesContentType {
+  paragraph: string | null;
+  title: string | null;
+}
+
+// home services
+
+export interface HomeServicesType {
+  image: string | null;
+  link: string | null;
+  text: string | null;
+  title: string | null;
+}
+//Double Cards
+
+export interface DoubleCardsDarkType {
+  imageDark: string;
+  imageRQ: string | null;
+  link: string | null;
+  title: string | null;
+}
+
+export interface DoubleCardsLightType {
+  imageLight: string;
+  imageSF: string | null;
+  link: string | null;
+  title: string | null;
+}
 // sub-categories
 
 //Accessibility
