67710d4c85246dcb98835078c71e93661dd2f963
Saba Phruidze <sabaphruidze01@gmail.com>
2024-06-24 22:50:10 +0400

    I think I corrected it well so header part visually finished

--- src/style/Themes.tsx
+++ src/style/Themes.tsx
@@ -1,12 +1,13 @@
 export const defaultTheme = {
   lightGray: "#c3c3c3",
-  purple: "#aa7eca",
+  Purple: "#aa7eca",
+  White: "#FFFFFF",
+  Black: "#000000",
+  Transparent: "rgba(0, 0, 0, 0)",
   DarkBlue: "#102948",
   MediumBlue: "#1d6a8c",
   LightBlue: "#93b6ce85",
   DarkGrey: "#595D6E",
   LightGrey: "#f7f8f9",
-  White: "#FFFFFF",
-  Black: "#000000",
   SkyNeon: "#00d4eb",
 };
