eae52b9986e3694c030ece6bedf76b59494aca96
Saba Phruidze <sabaphruidze01@gmail.com>
2024-06-29 23:44:30 +0400

    I corrected texts in the firebase and I think only left the images for the home page, so I will move on the login and register page to make diversity of them

---
+++ src/sub-category/CommercialDigitalPrinting.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CommercialDigitalPrinting() {
+  return <div>CommercialDigitalPrinting</div>;
+}
---
+++ src/sub-category/CommercialOffsetPrinting.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CommercialOffsetPrinting() {
+  return <div>CommercialOffsetPrinting</div>;
+}
