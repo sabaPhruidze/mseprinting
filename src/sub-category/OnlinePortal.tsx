eafcf832fb5c87bf3246c75a43f884caa3afa725
Saba Phruidze <sabaphruidze01@gmail.com>
2024-07-31 21:32:24 +0400

    I added links on carousel so it will move on each individual page which I will update

---
+++ src/sub-category/OnlinePortal.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function OnlinePortal() {
+  return <div>OnlinePortal</div>;
+}
---
+++ src/sub-category/PrintingCopying.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PrintingCopying() {
+  return <div>PrintingCopying</div>;
+}
