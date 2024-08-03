c8c1b914581fe954657a306c6beb5548041c8fd6
Saba Phruidze <sabaphruidze01@gmail.com>
2024-08-03 23:08:40 +0400

    I did same on direct mailing and next will be signs

--- src/sub-category/DirectMailing.tsx
+++ src/sub-category/DirectMailing.tsx
@@ -1,5 +1,43 @@
-import React from "react";
+// components/DirectMailing.tsx
+
+import { useState, useEffect, useMemo } from "react";
+import {
+  GlobalContainerColumn,
+  GlobalBoxColumnStart,
+  Globalh2TitleWithMB20,
+  GlobalPart,
+  GlobalMainTitle,
+} from "../style/GlobalStyle";
+import { fetchDirectMailingData } from "../data/sub-category data/AllSubCategoryData";
+import { DirectMailDocument } from "../types/DataTypes";
 
 export default function DirectMailing() {
-  return <div>DirectMailing</div>;
+  const [directMailingData, setDirectMailingData] =
+    useState<DirectMailDocument | null>(null);
+
+  useEffect(() => {
+    const getDirectMailingData = async () => {
+      const data = await fetchDirectMailingData();
+      if (data) {
+        setDirectMailingData(data);
+      }
+    };
+
+    getDirectMailingData();
+  }, []);
+
+  const memoizedData = useMemo(() => directMailingData, [directMailingData]);
+
+  return (
+    <GlobalContainerColumn>
+      <GlobalMainTitle>{memoizedData?.one.title}</GlobalMainTitle>
+
+      {memoizedData?.two.map((item, index) => (
+        <GlobalBoxColumnStart key={index}>
+          <Globalh2TitleWithMB20>{item.title}</Globalh2TitleWithMB20>
+          {item.content && <GlobalPart>{item.content}</GlobalPart>}
+        </GlobalBoxColumnStart>
+      ))}
+    </GlobalContainerColumn>
+  );
 }
