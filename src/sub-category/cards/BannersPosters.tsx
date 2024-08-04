ebbb97d5c45b545b2552f278b510314182e84d95
Saba Phruidze <sabaphruidze01@gmail.com>
2024-08-04 17:53:19 +0400

    I added new components which will resamble to cards git push

---
+++ src/sub-category/cards/BannersPosters.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function BannersPosters() {
+  return <div>BannersPosters</div>;
+}
---
+++ src/sub-category/cards/LabelsPackaging.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function LabelsPackaging() {
+  return <div>LabelsPackaging</div>;
+}
