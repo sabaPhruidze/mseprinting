a4cede3df7ba72430ff85d173f6a34f22d0b6dd8
Saba Phruidze <sabaphruidze01@gmail.com>
2024-08-20 22:28:45 +0400

    I have to increase it's speed

--- src/data/CarouselData.tsx
+++ src/data/CarouselData.tsx
@@ -1,12 +1,12 @@
 import { CarouselType } from "../types/DataTypes";
 import { GeneralizedFetch } from "../importantparts/GeneralizedFetch";
 // Import the images
-import digitalPrinting from "../assets/img/carousel/DIGITAL_PRINTING.jpeg";
-import directMail from "../assets/img/carousel/DIRECT_MAIL.jpeg";
-import offsetPrinting from "../assets/img/carousel/OFFSET_PRINTING.jpeg";
-import onlineOrdering from "../assets/img/carousel/ONLINE_ORDERING.jpeg";
-import printingCopying from "../assets/img/carousel/PRINTING_COPYING.jpeg";
-import signs from "../assets/img/carousel/SIGNS.jpeg";
+import digitalPrinting from "../assets/img/carousel/DIGITAL_PRINTING.png";
+import directMail from "../assets/img/carousel/DIRECT_MAIL.png";
+import offsetPrinting from "../assets/img/carousel/OFFSET_PRINTING.png";
+import onlineOrdering from "../assets/img/carousel/ONLINE_ORDERING.png";
+import printingCopying from "../assets/img/carousel/PRINTING_COPYING.png";
+import signs from "../assets/img/carousel/SIGNS.png";
 
 export const fetchCarouselData = async (): Promise<CarouselType[]> => {
   const data = await GeneralizedFetch<{ data: CarouselType[] }>(
