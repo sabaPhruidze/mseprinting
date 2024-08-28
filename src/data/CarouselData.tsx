a6075d4f94cc8afb5f84047aca2ddca29c8733d0
Saba Phruidze <sabaphruidze01@gmail.com>
2024-08-28 21:35:08 +0400

    I resized the images and I think the speed has been increased

--- src/data/CarouselData.tsx
+++ src/data/CarouselData.tsx
@@ -1,9 +1,9 @@
 import { CarouselType } from "../types/DataTypes";
 import { GeneralizedFetch } from "../importantparts/GeneralizedFetch";
 // Import the images
-import digitalPrinting from "../assets/img/carousel/DIGITAL_PRINTING.png";
+import digitalPrinting from "../assets/img/carousel/DIGITAL_PRINTING.jpg";
 import directMail from "../assets/img/carousel/DIRECT_MAIL.png";
-import offsetPrinting from "../assets/img/carousel/OFFSET_PRINTING.png";
+import offsetPrinting from "../assets/img/carousel/OFFSET_PRINTING.jpg";
 import onlineOrdering from "../assets/img/carousel/ONLINE_ORDERING.png";
 import printingCopying from "../assets/img/carousel/PRINTING_COPYING.png";
 import signs from "../assets/img/carousel/SIGNS.png";
