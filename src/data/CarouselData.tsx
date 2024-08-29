94284dd96af1b874bc7962dab472be52889feb15
Saba Phruidze <sabaphruidze01@gmail.com>
2024-08-29 21:56:54 +0400

    I think images will be more serchable

--- src/data/CarouselData.tsx
+++ src/data/CarouselData.tsx
@@ -1,5 +1,6 @@
 import { CarouselType } from "../types/DataTypes";
 import { GeneralizedFetch } from "../importantparts/GeneralizedFetch";
+
 // Import the images
 import digitalPrinting from "../assets/img/carousel/DIGITAL_PRINTING.jpg";
 import directMail from "../assets/img/carousel/DIRECT_MAIL.png";
@@ -16,11 +17,66 @@ export const fetchCarouselData = async (): Promise<CarouselType[]> => {
   return data ? data.data : [];
 };
 
+// Define metadata for each image
 export const CAROUSEL_DATA = [
-  offsetPrinting,
-  digitalPrinting,
-  printingCopying,
-  directMail,
-  signs,
-  onlineOrdering,
+  {
+    src: offsetPrinting,
+    alt: "Offset Printing",
+    title: "Offset Printing Services",
+    geoData: {
+      latitude: "44.9778",
+      longitude: "-93.2650",
+      location: "Minneapolis",
+    },
+  },
+  {
+    src: digitalPrinting,
+    alt: "Digital Printing",
+    title: "Digital Printing Services",
+    geoData: {
+      latitude: "44.9778",
+      longitude: "-93.2650",
+      location: "Minneapolis",
+    },
+  },
+  {
+    src: printingCopying,
+    alt: "Printing and Copying",
+    title: "Printing and Copying Services",
+    geoData: {
+      latitude: "44.9778",
+      longitude: "-93.2650",
+      location: "Minneapolis",
+    },
+  },
+  {
+    src: directMail,
+    alt: "Direct Mail",
+    title: "Direct Mail Services",
+    geoData: {
+      latitude: "44.9778",
+      longitude: "-93.2650",
+      location: "Minneapolis",
+    },
+  },
+  {
+    src: signs,
+    alt: "Signs",
+    title: "Signage Services",
+    geoData: {
+      latitude: "44.9778",
+      longitude: "-93.2650",
+      location: "Minneapolis",
+    },
+  },
+  {
+    src: onlineOrdering,
+    alt: "Online Ordering",
+    title: "Online Ordering Services",
+    geoData: {
+      latitude: "44.9778",
+      longitude: "-93.2650",
+      location: "Minneapolis",
+    },
+  },
 ];
