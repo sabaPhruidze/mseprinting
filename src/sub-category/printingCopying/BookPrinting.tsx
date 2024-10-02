187d4a194fa9b9bfac57250c084f7cb0d9666e15
Saba Phruidze <sabaphruidze01@gmail.com>
2024-10-02 19:51:00 +0400

    I added  one page

---
+++ src/sub-category/printingCopying/BookPrinting.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function BookPrinting() {
+  return <div>BookPrinting</div>;
+}
---
+++ src/sub-category/printingCopying/LabelsStickersDecals.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function LabelsStickersDecals() {
+  return <div>LabelsStickersDecals</div>;
+}
