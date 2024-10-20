623a4977268a0779f6a07d63c9e26b339052749b
Saba Phruidze <sabaphruidze01@gmail.com>
2024-10-20 18:32:43 +0400

    I finished adding texts now I have to change each page image but before that I will add all pages in search engine

--- src/sub-category/fulfillmentServices/PickPack.tsx
+++ src/sub-category/fulfillmentServices/PickPack.tsx
@@ -1,5 +1,65 @@
-import React from "react";
+import { useState, useEffect, useMemo } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  GlobalContainerColumn,
+  GlobalPart,
+  GlobalTextContainer,
+  FullBackgroundContainerZERO,
+  TitleAndButtonContainer,
+  FullScreenTitle,
+  FullScreenButton,
+  GlobalMainContent,
+} from "../../style/GlobalStyle";
+import { fetchPickPackData } from "../../data/sub-category data/AllSubCategoryData";
+import ImageWithSEO from "../../importantparts/ImageWithCEO";
+import { ADA_WAYFINDING_SIGNS_IMAGE } from "../../data/sub-category data/ImageWithCEOData"; // Assuming there's no separate image yet
+import { SubCategoryCommonTypes } from "../../types/DataTypes";
 
 export default function PickPack() {
-  return <div>PickPack</div>;
+  const [pickPackData, setPickPackData] =
+    useState<SubCategoryCommonTypes | null>(null);
+
+  useEffect(() => {
+    const getPickPackData = async () => {
+      const data = await fetchPickPackData();
+      if (data) {
+        setPickPackData(data);
+      }
+    };
+
+    getPickPackData();
+  }, []);
+
+  const memoizedData = useMemo(() => pickPackData, [pickPackData]);
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <FullBackgroundContainerZERO>
+        <div className="black-overlay"></div>
+        <ImageWithSEO
+          src={ADA_WAYFINDING_SIGNS_IMAGE.src}
+          alt={ADA_WAYFINDING_SIGNS_IMAGE.alt}
+          title={ADA_WAYFINDING_SIGNS_IMAGE.title}
+          geoData={ADA_WAYFINDING_SIGNS_IMAGE.geoData}
+          loading="eager"
+        />
+        <TitleAndButtonContainer>
+          <FullScreenTitle>{memoizedData?.one?.title}</FullScreenTitle>
+          <GlobalMainContent>{memoizedData?.one?.content}</GlobalMainContent>
+          <FullScreenButton onClick={() => navigate("/request-quote")}>
+            {memoizedData?.one?.button}
+          </FullScreenButton>
+        </TitleAndButtonContainer>
+      </FullBackgroundContainerZERO>
+
+      <GlobalContainerColumn>
+        <GlobalTextContainer>
+          {memoizedData?.two?.map((item, index) => (
+            <GlobalPart key={index}>{item}</GlobalPart>
+          ))}
+        </GlobalTextContainer>
+      </GlobalContainerColumn>
+    </div>
+  );
 }
