3f5c49000997eda87adbb25523e1a169ef395d68
Saba Phruidze <sabaphruidze01@gmail.com>
2024-10-27 21:25:38 +0400

    I corrected searchbility error

--- src/importantparts/SearchEngine.tsx
+++ src/importantparts/SearchEngine.tsx
@@ -21,20 +21,22 @@ const SearchEngine: React.FC = () => {
   if (!searchContext) {
     throw new Error("rootContext must be used within a Root provider");
   }
+
   const { state, dispatching } = searchContext;
   const { SearchQuery } = state;
 
   useEffect(() => {
+    // Fetch data on component mount
     const fetchData = async () => {
       const result = await fetchSearchEngineData();
       if (result) {
         setData(result);
       }
     };
-
     fetchData();
   }, []);
 
+  // Handle search filtering based on user input
   const handleSearch = useCallback(() => {
     const filteredResults = data.filter((result) =>
       result.title.toLowerCase().includes(SearchQuery.toLowerCase())
@@ -43,6 +45,7 @@ const SearchEngine: React.FC = () => {
     dispatching("SEARCH_DONE", true);
   }, [SearchQuery, data, dispatching]);
 
+  // Handle search input changes
   const handleChange = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
       dispatching("SEARCH_QUERY_CHANGE", e.target.value);
